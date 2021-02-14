3dec16b26715807f2241424567cce03814e9f452
ryrimnd <78997078+ryrimnd@users.noreply.github.com>
2021-02-14 21:46:24 +0700

    Add files via upload

---
+++ Tugas-1/Soal-2.py
@@ -0,0 +1,10 @@
+Π = 22/7
+r = input("Nilai jari-jari lingkaran: ")
+rr = int(r)
+l = rr * rr * Π
+
+#print(rr)
+#print(l)
+
+#print(f"Luas lingkaran dengan- jari-jari {rr} cm adalah {l} cm\u00b2")
+print(f"Luas lingkaran dengan- jari-jari {rr} cm adalah {l:.2f} cm\u00b2")
