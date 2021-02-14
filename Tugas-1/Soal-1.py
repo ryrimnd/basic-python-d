d4682d6fe94373753b6af749e4081f48a765e1d9
ryrimnd <78997078+ryrimnd@users.noreply.github.com>
2021-02-14 23:29:32 +0700

    Delete Tugas-1 directory

--- Tugas-1/Soal-1.py
+++
@@ -1,9 +0,0 @@
-name = "Rayhan"
-age = int(19)
-height = float(173.5)
-
-#print(type(name))
-#print(type(age))
-#print(type(height))
-
-print(f"Nama saya {name}, umur saya {age} tahun dan tinggi saya {height} cm.")
